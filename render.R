419ce33685f89fba63ff864e22bf9ce7be391dda
JulienGAMartin <julien.ga.martin@gmail.com>
2020-09-04 00:19:37 -0400

    render all

--- render.R
+++ render.R
@@ -1,9 +1,7 @@
 #!/usr/bin/env Rscript
-rm(list=ls())
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+rm(list=ls());bookdown::render_book("index.Rmd", "bookdown::gitbook")
 rm(list=ls());bookdown::render_book("index.Rmd", "bookdown::pdf_book")
-rm(list=ls())
-bookdown::render_book("index.Rmd", 'bookdown::epub_book')
+rm(list=ls());bookdown::render_book("index.Rmd", 'bookdown::epub_book')
 bookdown::calibre('docs/Labo_BIO4558.epub', 'mobi')
 
 bookdown::preview_chapter("06-anova_mult.Rmd", output_dir = "docs/preview")
