93b44ff097dc93b152ad319bf45a986936300fca
JulienGAMartin <julien.ga.martin@gmail.com>
2020-09-03 22:27:11 -0400

    better rendering with style

--- render.R
+++ render.R
@@ -1,3 +1,11 @@
-rm(list=ls());
+#!/usr/bin/env Rscript
+
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+bookdown::render_book("index.Rmd", 'bookdown::epub_book')
+bookdown::calibre('docs/Labo_BIO4558.epub', 'mobi')
+
+bookdown::preview_chapter("06-anova_mult.Rmd", output_dir = "docs/preview")
+#bookdown::preview_chapter("06-anova_mult.Rmd", output_format = 'bookdown::html_chapters', output_dir = "docs/preview")
+
+bookdown::preview_chapter("10-notebook.Rmd", output_dir = "docs/preview")
