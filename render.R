e6cdcb71cad5951f4ac9928d1db1a8d186ae6d33
JulienGAMartin <julien.ga.martin@gmail.com>
2020-10-07 18:46:45 -0400

    knit with chap 3

--- render.R
+++ render.R
@@ -7,4 +7,4 @@ bookdown::calibre('docs/Labo_BIO4558.epub', 'mobi')
 bookdown::preview_chapter("06-anova_mult.Rmd", output_dir = "docs/preview")
 #bookdown::preview_chapter("06-anova_mult.Rmd", output_format = 'bookdown::html_chapters', output_dir = "docs/preview")
 
-bookdown::preview_chapter("02-Gpower.Rmd", output_dir = "docs/preview")
+bookdown::preview_chapter("03-reg_lin.Rmd", output_dir = "docs/preview")
