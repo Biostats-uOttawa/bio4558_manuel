0e11b80bacc6fd66c7076522beb5725adcfaf0dd
JulienGAMartin <julien.ga.martin@gmail.com>
2020-10-09 17:34:23 -0400

    first draft 6-9

--- render.R
+++ render.R
@@ -4,7 +4,10 @@ rm(list=ls());bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 rm(list=ls());bookdown::render_book("index.Rmd", 'bookdown::epub_book')
 bookdown::calibre('docs/Labo_BIO4558.epub', 'mobi')
 
-bookdown::preview_chapter("06-anova_mult.Rmd", output_dir = "docs/preview")
-#bookdown::preview_chapter("06-anova_mult.Rmd", output_format = 'bookdown::html_chapters', output_dir = "docs/preview")
 
+#bookdown::preview_chapter("06-anova_mult.Rmd", output_format = 'bookdown::html_chapters', output_dir = "docs/preview")
 bookdown::preview_chapter("03-reg_lin.Rmd", output_dir = "docs/preview")
+bookdown::preview_chapter("06-anova_mult.Rmd", output_dir = "docs/preview")
+bookdown::preview_chapter("07-reg_mult.Rmd", output_dir = "docs/preview")
+bookdown::preview_chapter("08-ancova_glm.Rmd", output_dir = "docs/preview")
+bookdown::preview_chapter("09-model_freq.Rmd", output_dir = "docs/preview")
