46524bed8aedde41e5ca13be5d67a5c9ce178572
JulienGAMartin <julien.ga.martin@gmail.com>
2020-10-28 21:39:07 -0400

    fixing solution problem

--- render.R
+++ render.R
@@ -6,6 +6,8 @@ bookdown::calibre('docs/Labo_BIO4558.epub', 'mobi')
 
 
 #bookdown::preview_chapter("06-anova_mult.Rmd", output_format = 'bookdown::html_chapters', output_dir = "docs/preview")
+bookdown::preview_chapter("01-introR_3.Rmd", output_dir = "docs/preview")
+bookdown::preview_chapter("02-Gpower.Rmd", output_dir = "docs/preview")
 bookdown::preview_chapter("03-reg_lin.Rmd", output_dir = "docs/preview")
 bookdown::preview_chapter("04-t_test.Rmd", output_dir = "docs/preview")
 bookdown::preview_chapter("05-anova.Rmd", output_dir = "docs/preview")
